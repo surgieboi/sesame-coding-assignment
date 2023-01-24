14d9774fb91c84ec011c80e682c2c72312cdf9ff
Sergio Villasenor <42443963+surgieboi@users.noreply.github.com>
2023-01-24 15:05:36 -0800

    update meta component

--- components/meta/meta.js
+++ components/meta/meta.js
@@ -10,7 +10,7 @@ export default function Meta() {
             <link rel="icon" href="/favicon.ico" />
             <meta property="og:title" content="Web3 Rewards | Discord and Wagmi OAuth2 Stater Kit" />
             <meta property="og:type" content="article" />
-            <meta property="og:url" content="https://sesame-coding-assignment.vercel.app/og.jpg" />
+            <meta property="og:url" content="https://sesame-coding-assignment.vercel.app/" />
             <meta property="og:image" content="https://sesame-coding-assignment.vercel.app/og.jpg" />
             <meta name="twitter:title" content="Web3 Rewards | Discord and Wagmi OAuth2 Stater Kit" />
             <meta name="twitter:description" content="Web3 Rewards Starter Kit using Discord, Next.js, and Wagmi." />
