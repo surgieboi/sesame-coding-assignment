4fbadd3348d35fe6e2aafb28687b9d0fd95deb46
Sergio Villasenor <42443963+surgieboi@users.noreply.github.com>
2023-01-24 14:52:26 -0800

    update wagmi to include walletconnect

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import { mainnet } from 'wagmi/chains'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 
 const { chains, provider, webSocketProvider } = configureChains(
   [mainnet],
@@ -17,6 +18,12 @@ const client = createClient({
   autoConnect: true,
   connectors: [
     new MetaMaskConnector({ chains }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        qrcode: true,
+      },
+    }),
   ],
   provider,
   webSocketProvider,
